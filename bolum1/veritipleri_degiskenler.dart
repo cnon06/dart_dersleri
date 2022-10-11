7501539101acd5d580ff99b62154a191d4408cd8
Sinan Alagöz <sinanalagoz74@gmail.com>
2022-10-11 12:13:30 +0300

    Initial commit

---
+++ bolum1/veritipleri_degiskenler.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  num sayi1 = 1;
+  num sayi2 = 1.3;
+  double sayi3 = 4.5;
+  int sayi4 = 7;
+  String ad = "Sinan";
+  bool erkekMi = true;
+  bool kadinMi = false;
+
+  print("Ad: " + ad);
+
+  var okul = "hacettepe";
+  var okulNo = 56;
+}
