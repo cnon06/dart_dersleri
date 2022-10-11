6825e20c1b21ed68cf53ff7f48b7daccf841177c
Sinan Alagöz <sinanalagoz74@gmail.com>
2022-10-11 12:45:13 +0300

    1
    2

--- bolum1/num_boolean.dart
+++ bolum1/num_boolean.dart
@@ -1,4 +1,32 @@
-
 void main(List<String> args) {
-  
+  int yas = 34;
+  print(yas);
+  yas = 44;
+  print(yas);
+
+  num yil = 1988;
+  print(yil);
+
+  double sayi = 53;
+  print(sayi);
+
+  int kilo = 84.6.toInt();
+  print(kilo);
+
+  dynamic deger = 65;
+  deger = "sinan";
+
+  int numara;
+  numara = 6;
+  print(numara);
+
+  int? numara2 = null;
+
+  print(numara2);
+
+  numara2 = 8;
+  print(numara + numara2);
+
+  int hexaDecimalNmuber = 0xAA11BB;
+  print(hexaDecimalNmuber);
 }
---
+++ bolum1/starter.dart
@@ -0,0 +1,3 @@
+void main(List<String> args) {
+  print("Program has been started from here");
+}
